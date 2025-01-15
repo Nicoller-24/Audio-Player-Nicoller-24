c883e4c8b66800785a36463f092239c5ac03c2e4
Nicolle Rodríguez Laytón <166450090+Nicoller-24@users.noreply.github.com>
2025-01-15 22:37:19 +0000

    componente payer creado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,19 @@
 import React from "react";
+import Player from "./Player";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<section className="text-center container bg-dark text-white">
+
+			<section>
+				Playlist
+			</section>
+			<section>
+				<Player/>
+			</section>
+
+		</section>
 	);
 };
 
